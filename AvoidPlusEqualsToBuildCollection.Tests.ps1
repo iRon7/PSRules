281068e77ad32ddd37c83087df8de5b38be62c72
iRon7 <boderonald@gmail.com>
2023-08-24 12:13:11 +0200

    Add files via upload

---
+++ AvoidPlusEqualsToBuildCollection.Tests.ps1
@@ -0,0 +1,143 @@
+#Requires -Modules @{ModuleName="Pester"; ModuleVersion="5.0.0"}
+
+Describe 'AvoidPlusEqualsToBuildCollections' {
+
+    BeforeAll {
+        $RulePath = $PSCommandPath.Replace('.Tests.ps1', '.psm1')
+        $RuleName = [io.path]::GetFileNameWithoutExtension($RulePath)
+
+        function Test-Rule {
+            Param(
+                [Parameter(Mandatory=$true, Position=0, ValueFromPipeline=$true)]
+                [ScriptBlock] $ScriptBlock
+            )
+            $Result = Invoke-ScriptAnalyzer -CustomRulePath $RulePath -ScriptDefinition $ScriptBlock.ToString()
+            $Result.Count -eq 1 -and $Result.RuleName -eq $RuleName
+        }
+    }
+
+    Context 'Positives' {
+        It 'For' {
+            {
+                $Test = @()
+                for ($Index = 0; $Index -lt 9; $Index++) {
+                    $Test += [PSCustomObject]@{ Index = $Index }
+                }
+            } | Test-Rule | Should -BeTrue
+        }
+        It 'Foreach' {
+            {
+                $Test = @()
+                foreach ($Index in 1,2,3) {
+                    $Test += [PSCustomObject]@{ Index = $Index }
+                }
+            } | Test-Rule | Should -BeTrue
+        }
+        It 'Foreach method' {
+            {
+                $Test = @()
+                @(1,2,3).foreach{
+                    $Test += [PSCustomObject]@{ Index = $Index }
+                }
+            } | Test-Rule | Should -BeTrue
+        }
+        It 'Foreach-Object' {
+            {
+                $Test = @()
+                1,2,3 | ForEach-Object {
+                    $Test += [PSCustomObject]@{ Index = $_ }
+                }
+            } | Test-Rule | Should -BeTrue
+        }
+        It 'Foreach-Object -Begin' {
+            {
+                1,2,3 | ForEach-Object -Begin { $Test = @() } -Process {
+                    $Test += [PSCustomObject]@{ Index = $_ }
+                }
+            } | Test-Rule | Should -BeTrue
+        }
+    It 'Foreach-Object { ... } { ... }' {
+            {
+                1,2,3 | ForEach-Object -Begin { $Test = @() } -Process {
+                    $Test += [PSCustomObject]@{ Index = $_ }
+                }
+            } | Test-Rule | Should -BeTrue
+        }
+    }
+
+    Context 'Positive use cases' {
+        It 'PSScriptAnalyzer' { # https://github.com/PowerShell/PSScriptAnalyzer/issues/1933
+            {
+                $knownProperties = @( )
+                Get-ChildItem -Path . -Recurse -Filter pom.xml | ForEach-Object {
+                    [xml]$pom = Get-Content $_
+                    $knownProperties += $pom.project.properties.getEnumerator() |
+                        Where-Object name -ne '#comment' |
+                        ForEach-Object name
+                    }
+            } | Test-Rule | Should -BeTrue
+        }
+    }
+
+    Context 'Positive pitfalls' {
+        It 'Space in @( )' {
+            {
+                $knownProperties = @( )
+                Get-ChildItem -Path . -Recurse -Filter pom.xml | ForEach-Object {
+                    [xml]$pom = Get-Content $_
+                    $knownProperties += $pom.project.properties.getEnumerator() |
+                        Where-Object name -ne '#comment' |
+                        ForEach-Object name
+                    }
+            } | Test-Rule | Should -BeTrue
+        }
+    }
+
+    Context 'Negatives' {
+        It 'Controlled' {
+            {
+                $a = @()
+                $a += 1
+                $a += 2
+            } | Test-Rule | Should -BeFalse
+        }
+        It 'Tainted' {
+            {
+                $Test = @()
+                for ($Index = 0; $Index -lt 9; $Index++) {
+                    $Test = @()
+                    $Test += [PSCustomObject]@{ Index = $Index }
+                }
+            } | Test-Rule | Should -BeFalse
+        }
+        It 'Not defined For' {
+            {
+                for ($Index = 0; $Index -lt 9; $Index++) {
+                    $Test += [PSCustomObject]@{ Index = $Index }
+                }
+            } | Test-Rule | Should -BeFalse
+        }
+        It 'Not defined ForEach-Object' {
+            {
+                Get-ChildItem -Path . -Recurse -Filter pom.xml | ForEach-Object {
+                    [xml]$pom = Get-Content $_
+                    $knownProperties += $pom.project.properties.getEnumerator() |
+                        Where-Object name -ne '#comment' |
+                        ForEach-Object name
+                    }
+            } | Test-Rule | Should -BeFalse
+        }
+        It 'Not an array ForEach-Object' {
+            {
+                $unknownProperties = @()
+                $knownProperties = ''
+                Get-ChildItem -Path . -Recurse -Filter pom.xml | ForEach-Object {
+                    [xml]$pom = Get-Content $_
+                    $knownProperties += $pom.project.properties.getEnumerator() |
+                        Where-Object name -ne '#comment' |
+                        ForEach-Object name
+                    }
+            } | Test-Rule | Should -BeFalse
+        }
+    }
+}
---
+++ AvoidPlusEqualsToBuildCollection.psm1
@@ -0,0 +1,141 @@
+#Requires -Version 3.0
+
+using namespace System.Management.Automation.Language
+
+function Measure-AvoidPlusEqualsToBuildCollection {
+<#
+    .SYNOPSIS
+    Avoid using the Assignment by Addition Operator to build collections
+    .DESCRIPTION
+    Array addition is inefficient because arrays have a fixed size. Each addition to the array
+    creates a new array big enough to hold all elements of both the left and right operands.
+    The elements of both operands are copied into the new array. For small collections, this
+    overhead may not matter. Performance can suffer for large collections.
+    See also: https://learn.microsoft.com/powershell/scripting/dev-cross-plat/performance/script-authoring-considerations
+    .INPUTS
+    [System.Management.Automation.Language.ScriptBlockAst]
+    .OUTPUTS
+    [Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord]
+#>
+
+    [CmdletBinding()]
+    [OutputType([Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord])]
+    Param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.Language.ScriptBlockAst]
+        $ScriptBlockAst
+    )
+    Begin {
+        function IsAssigned ([String]$VariableName, [Ast]$Statement, [Switch]$Empty) {
+            If ($Statement -is [NamedBlockAst] -or $Statement -is [StatementBlockAst]) {
+                $Container = $Statement
+                $Index = $Container.Statements.Count
+            }
+            else {
+                $Container = $Statement.Parent
+                if ($Container -isnot [NamedBlockAst]) { Return $false }
+                $Index = 0
+                While ($Container.Statements[$Index].Extent.StartOffset -lt $Statement.Extent.StartOffset) { $Index++ }
+            }
+            $EqualsStatement = $Null
+            While (--$Index -ge 0) {
+                $EqualsStatement = $Container.Statements[$Index]
+                if (
+                    $EqualsStatement -is [AssignmentStatementAst] -and
+                    $EqualsStatement.Operator -eq 'Equals' -and
+                    $EqualsStatement.Left.VariablePath.UserPath -eq $VariableName
+                ) { break }
+            }
+            if ($Index -lt 0) { return $false }
+            if ($Empty) {
+                return (
+                    $EqualsStatement.Right.Expression -is [ArrayExpressionAst] -and
+                    !$EqualsStatement.Right.Expression.SubExpression.Extent.Text
+                )
+            } else { return $true }
+        }
+    }
+    Process {
+        [ScriptBlock]$Predicate = {
+            Param ([Ast]$Ast)
+            if ($Ast -isnot [AssignmentStatementAst] -or $Ast.Operator -ne 'PlusEquals') { return $false }
+            $VariableName = $Ast.Left.VariablePath.UserPath
+            $BlockAst = $Ast.Parent
+            if ($BlockAst -is [StatementBlockAst]) {
+                if (IsAssigned -Empty $VariableName $BlockAst) { return $false } # Controlled
+                $StatementAst = $BlockAst.Parent
+                if     ($StatementAst -is [ForStatementAst])     { return (IsAssigned -Empty $VariableName $StatementAst) }
+                elseif ($StatementAst -is [ForEachStatementAst]) { return (IsAssigned -Empty $VariableName $StatementAst) }
+                return $false
+            }
+            elseif ($BlockAst -is [NamedBlockAst]) {
+                if (IsAssigned -Empty $VariableName $BlockAst) { return $false } # Controlled
+                $ScriptBlockAst = $BlockAst.Parent
+                if ($ScriptBlockAst -isnot [ScriptBlockAst]) { Return $false }
+                $ScriptBlockExpressionAst = $ScriptBlockAst.Parent
+                if ($ScriptBlockExpressionAst -isnot [ScriptBlockExpressionAst]) { return $false }
+                $ExpressionAst = $ScriptBlockExpressionAst.Parent
+                if ($ExpressionAst -is [InvokeMemberExpressionAst]) {
+                    if ($ExpressionAst.Member.Value -ne 'foreach') { return $false }
+                    if (IsAssigned -Empty $VariableName $ExpressionAst) { return $false } # Controlled
+                    $CommandExpressionAst = $ExpressionAst.Parent
+                    if ($CommandExpressionAst -isnot [CommandExpressionAst]) { return $false }
+                    $PipelineAst = $CommandExpressionAst.Parent
+                    if ($PipelineAst -isnot [PipelineAst]) { return $false }
+                    return (IsAssigned -Empty $VariableName $PipelineAst)
+                }
+                elseif($ExpressionAst -is [CommandAst]) {
+                    $CommandAst = $ScriptBlockExpressionAst.Parent
+                    if ($CommandAst -isnot [CommandAst]) { return $false }
+                    $Parameters = @{}
+                    $CommandElement = $Null
+                    $ParameterIndex = 0
+                    foreach ($Element in $CommandAst.CommandElements) {
+                        if (!$CommandElement -and $Element -is [StringConstantExpressionAst]) {
+                            $CommandElement = $Element
+                        }
+                        elseif ($Element -is [ScriptBlockExpressionAst]) {
+                            if ($ParameterName) { $Parameters[$ParameterName] = $Element }
+                            else { $Parameters[$ParameterIndex++] = $Element }
+                        }
+                        $ParameterName = if ($Element -is [CommandParameterAst]) { $Element.ParameterName }
+                    }
+                    if ($CommandElement -in 'ForEach-Object', 'foreach', '%') {
+                        if (!$Parameters.ContainsKey('Process')) {
+                            if ($Parameters.ContainsKey('Begin') -and $Parameters.ContainsKey(1)) {
+                                $Parameters['Begin']   = $Parameters[0]
+                                $Parameters['Process'] = $Parameters[1]
+                            }
+                            elseif ($Parameters.ContainsKey(0)) {
+                                $Parameters['Process'] = $Parameters[0]
+                            }
+                        }
+                        if ($Parameters['Process'].Extent.StartOffset -eq $ScriptBlockAst.Extent.StartOffset) {
+                            if ($Parameters.ContainsKey('Begin')) {
+                                IsAssigned -Empty $VariableName $Parameters['Begin'].ScriptBlock.EndBlock
+                            }
+                            $PipelineAst = $ExpressionAst.Parent
+                            if ($PipelineAst -isnot [PipelineAst]) { return $false }
+                            return (IsAssigned -Empty $VariableName $PipelineAst)
+                        }
+                    }
+                }
+            }
+            return $false
+        }
+
+        $Violations = $ScriptBlockAst.FindAll($Predicate, $false)
+        Foreach ($Violation in $Violations) {
+            [Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord]@{
+                Message              = "Avoid using the assignment by addition operator for building a collection."
+                Extent               = $Violation.Extent
+                RuleName             = 'AvoidPlusEqualsToBuildCollection'
+                Severity             = 'Warning'
+                RuleSuppressionID    = 'AvoidPlusEqualsToBuildCollection'
+                # SuggestedCorrections = 'link the  concerned Ivanti variable to the Ivanti module parameter and'
+            }
+        }
+    }
+}
+Export-ModuleMember -Function Measure-*
