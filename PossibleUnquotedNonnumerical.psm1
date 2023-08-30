aaf986e8e5c2121e4a2cb393f92549af047ebf78
iRon7 <boderonald@gmail.com>
2023-08-30 12:43:40 +0200

    Added rule

---
+++ PossibleUnquotedNonnumerical.psm1
@@ -0,0 +1,57 @@
+#Requires -Version 3.0
+
+function Measure-PossibleUnquotedNonnumerical {
+<#
+    .SYNOPSIS
+    Nonnumericals need to be quoted.
+    .DESCRIPTION
+    Nonnumericals (as version, date and ipaddress) types need to be quoted otherwise they
+    will be seen as a number (double type) with a property member after the second dot.
+    .INPUTS
+    [System.Management.Automation.Language.ScriptBlockAst]
+    .OUTPUTS
+    [Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord]
+    .LINK
+    https://github.com/PowerShell/PSScriptAnalyzer/issues/1698
+    https://github.com/PowerShell/PowerShell/issues/15756
+    
+#>
+
+    [CmdletBinding()]
+    [OutputType([Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord])]
+    Param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.Language.ScriptBlockAst]
+        $ScriptBlockAst
+    )
+    Process {
+        [ScriptBlock]$Predicate = {
+            Param ([System.Management.Automation.Language.Ast]$Ast)
+            $Ast -is [System.Management.Automation.Language.MemberExpressionAst] -and
+            $Ast.Extent.Text -Match '^\d\.\d(\.\d)+$'
+        }
+        $Violations = $ScriptBlockAst.FindAll($Predicate, $False)
+        Foreach ($Violation in $Violations) {
+            $Extent = $Violation.Extent
+            [Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord]@{
+                Message              = "Nonnumericals need to be quoted: '$Extent'"
+                Extent               = $Extent
+                RuleName             = 'PSPossibleUnquotedNonnumerical'
+                Severity             = 'Warning'
+                RuleSuppressionID    = $Extent.Text
+                SuggestedCorrections = [System.Collections.ObjectModel.Collection[Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.CorrectionExtent]](
+                    [Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.CorrectionExtent]::New(
+                        $Extent.StartLineNumber,
+                        $Extent.EndLineNumber,
+                        $Extent.StartColumnNumber,
+                        $Extent.EndColumnNumber,
+                        "'$Extent'",
+                        "Quote the nonnumerical: '$Extent'"
+                    )
+                )
+            }
+        }
+    }
+}
+Export-ModuleMember -Function Measure-*
